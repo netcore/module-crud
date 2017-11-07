d35ec30896e7084ec84d02ce0ee5b8305d640ab2
Kristers Zarins <kristerskz@gmail.com>
2017-11-07 12:53:05 +0200

    Make password required when storing a new user

--- Http/Requests/CRUDRequest.php
+++ Http/Requests/CRUDRequest.php
@@ -27,18 +27,21 @@ class CRUDRequest extends FormRequest
         /** @var CrudifyModel $model */
         $model = $this->route()->controller->getModel();
 
-        $userId = request()->route('user');
-        if($userId) {
+        $dealingWithUser = request()->segment(2) == 'users';
+
+        if($dealingWithUser) {
+
+            $userId = request()->route('user');
             $thisModel = $model->find($userId);
             $rules = $model->getValidationRules($thisModel);
+
+            $isStore = $userId ? false : true;
+            if(isset($rules['password']) AND $isStore) {
+                $rules['password'] .= '|required';
+            }
         } else {
             $thisModel = null;
             $rules = $model->getValidationRules($thisModel);
-        }
-
-        $isStore = $userId ? false : true;
-        if(isset($rules['password']) AND $isStore) {
-            $rules['password'] .= '|required';
         }
 
         return $rules;
